a88ee84fb16183b0fff74fb4ea66bf886116c73f
OctavioBerruti <octavioberruti8@gmail.com>
2016-10-30 23:34:15 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -8,32 +8,23 @@ class Boleto{
 	public $fecha;
 	public $tipoBoleto;
 	public $saldo;
-	public $lineabondi;
+	public $linea;
 	public $idCard;
 
 	
-	public function __construct (Transporte $transporte, $fecha_y_hora, $tipoBoleto, $saldoTarjeta, $cardid)
+	public function __construct (Transporte $transporte, $fecha_y_hora, $tipoBoleto, $saldoTarjeta, $cardid, $Linea)
 	{
 		$this->transporte = $transporte;
 		$this->fecha=$fecha_y_hora;
 		$this->tipoBoleto=$tipoBoleto;
 		$this->saldo=$saldoTarjeta;
 		$this->idCard = $cardid;
-		lineaa();
+		$this->linea = $Linea
 	}
-
-	public function lineaa(){
-                if($this->transporte->tipo=="colectivo"){
-                        $this->lineabondi = $this->transporte->linea;
-                }
-                else{
-                        $this->lineabondi = $this->transporte->patente;
-                }        
-        }
 	
 
     public function informacion(){
-    	print "La informacion del boleto es la siguiente: <br /> Linea o patente: ". $this->lineabondi . "<br />Fecha del boleto: " . $this->fecha . "<br /> Tipo de boleto: " . $this->tipoBoleto . "<br /> Saldo restante: " . $this->saldo . "<br /> Numero de tarjeta: " . $this->idCard ;
+    	print "La informacion del boleto es la siguiente: <br /> Linea o patente: ". $this->linea . "<br />Fecha del boleto: " . $this->fecha . "<br /> Tipo de boleto: " . $this->tipoBoleto . "<br /> Saldo restante: " . $this->saldo . "<br /> Numero de tarjeta: " . $this->idCard ;
     }
 
 }
