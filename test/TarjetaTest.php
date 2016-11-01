4be3c6c33c9643de54e24a168f793585a8272f46
OctavioBerruti <octavioberruti8@gmail.com>
2016-11-01 09:52:40 -0300

    Update TarjetaTest.php

--- test/TarjetaTest.php
+++ test/TarjetaTest.php
@@ -15,41 +15,41 @@ class TarjetaTest extends TestCase {
 
 
   public function testPagarViaje() {
-	$tarjeta = new tarjetaa;
-	$tarjeta->recargar(272);
+	$tarjeta1 = new tarjetaa;
+	$tarjeta1->recargar(272);
 	$colectivo144Negro = new Colectivo("144 Negro", "Rosario Bus");
-	$tarjeta->pagar($colectivo144Negro, "2016/06/30 22:50");
-	$this->assertEquals($tarjeta->saldo(), 320 - 8, "Cuando pago el pasaje me deberian quedar 312 pesos");	
+	$tarjeta1->pagar($colectivo144Negro, "2016/06/30 22:50");
+	$this->assertEquals($tarjeta1->saldo(), 320 - 8, "Cuando pago el pasaje me deberian quedar 312 pesos");	
 
   }
 
   public function testPagarViajeSinSaldo() {
-	$tarjeta = new tarjetaa;
+	$tarjeta2 = new tarjetaa;
 	$colectivo144Negro = new Colectivo("144 Negro", "Rosario Bus");
-	$this->assertEquals($tarjeta->pagar($colectivo144Negro, "2016/06/30 22:50"), false, "Cuando pago el pasaje no me deberia dejar pagar y deberia decir que no tengo saldo ");
+	$this->assertEquals($tarjeta2->pagar($colectivo144Negro, "2016/06/30 22:50"), false, "Cuando pago el pasaje no me deberia dejar pagar y deberia decir que no tengo saldo ");
   }
 
   public function testTransbordo() {
 	
-$tarjeta = new tarjetaa;
-$tarjeta->recargar(272);
+$tarjeta3 = new tarjetaa;
+$tarjeta3->recargar(272);
 $colectivo144Negro = new Colectivo("144 Negro", "Rosario Bus");
-$tarjeta->pagar($colectivo144Negro, "2016/06/30 22:50");
+$tarjeta3->pagar($colectivo144Negro, "2016/06/30 22:50");
 $colectivo135 = new Colectivo("135", "Rosario Bus");
-$tarjeta->pagar($colectivo135, "2016/06/30 23:10");
-$this->assertEquals($tarjeta->saldo(), (320 - 8 - 2.64), "Cuando pago el pasaje me deberian quedar 309.36 pesos");
+$tarjeta3->pagar($colectivo135, "2016/06/30 23:10");
+$this->assertEquals($tarjeta3->saldo(), (320 - 8 - 2.64), "Cuando pago el pasaje me deberian quedar 309.36 pesos");
 
   }
 
   public function testNoTransbordo() {
 
-	$tarjeta = new tarjetaa;
-$tarjeta->recargar(272);
+	$tarjeta4 = new tarjetaa;
+$tarjeta4->recargar(272);
 $colectivo144Negro = new Colectivo("144 Negro", "Rosario Bus");
-$tarjeta->pagar($colectivo144Negro, "2016/06/30 22:50");
+$tarjeta4->pagar($colectivo144Negro, "2016/06/30 22:50");
 $colectivo135 = new Colectivo("135", "Rosario Bus");
-$tarjeta->pagar($colectivo135, "2016/07/30 23:10");
-$this->assertEquals($tarjeta->saldo(), (320 - 8 - 8), "Cuando pago el pasaje me deberian quedar 304 pesos");
+$tarjeta4->pagar($colectivo135, "2016/07/30 23:10");
+$this->assertEquals($tarjeta4->saldo(), (320 - 8 - 8), "Cuando pago el pasaje me deberian quedar 304 pesos");
 
   }
 
